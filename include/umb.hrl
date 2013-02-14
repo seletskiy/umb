00c7efba0390e6606256c9a008bd92537f13c48c
Seletskiy Stanislav <s.seletskiy@office.ngs.ru>
2013-02-14 12:35:12 +0700

    umb.hrl: more constants from docs

--- include/umb.hrl
+++ include/umb.hrl
@@ -1,4 +1,5 @@
 -define(UMB_CMD_VERSION,      16#20).
+-define(UMB_CMD_INFO,         16#2D).
 -define(UMB_CMD_ONLINE_DATA,  16#23).
 -define(UMB_CMD_MULTICHANNEL, 16#2F).
 
@@ -9,13 +10,28 @@
 
 -define(UMB_VER_1_0, 16#10).
 
+-define(UMB_INF_CNL_NUM,  16#15).
+-define(UMB_INF_CNL_LIST, 16#16).
+-define(UMB_INF_CNL_FULL, 16#30).
+
 -define(UMB_DEV_R2S,     2).
 -define(UMB_DEV_MASTER, 15).
 
+-define(UMB_CNL_R2S_TEMP_C,       100).
+-define(UMB_CNL_R2S_LITRES,       600).
 -define(UMB_CNL_R2S_LITRES_SINCE, 601).
+-define(UMB_CNL_R2S_FILM,         610).
+-define(UMB_CNL_R2S_FILM_SINCE,   611).
 -define(UMB_CNL_R2S_PREC_TYPE,    700).
--define(UMB_CNL_R2S_SNOWFLAKES,   715).
-
+-define(UMB_CNL_R2S_DRIZZLES,     715).
+-define(UMB_CNL_R2S_RDROPS,       720).
+-define(UMB_CNL_R2S_SFLAKES,      725).
+-define(UMB_CNL_R2S_HAILS,        730).
+-define(UMB_CNL_R2S_DRIZZLE_F,    740).
+-define(UMB_CNL_R2S_RAIN_F,       745).
+-define(UMB_CNL_R2S_SNOW_F,       750).
+-define(UMB_CNL_R2S_HAIL_F,       755).
+-define(UMB_CNL_R2S_PREC_INT,     1053).
 
 -define(UMB_DATA_UCHAR,  16).
 -define(UMB_DATA_CHAR,   17).
